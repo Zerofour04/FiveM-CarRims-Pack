901c869b968b0779a203cf96c42b7b9285065845
Ben // Zerofour // 04 <brubiyt@gmail.com>
2022-03-23 17:48:42 +0100

    Uploaded Rims

---
+++ __resource.lua
@@ -0,0 +1,21 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+files {
+
+    'carcols.meta',
+    'dlctext.meta',
+    'jdmrimscontentunlocks.meta',
+    'content.xml',
+    'setup2.xml',
+    'global.gxt2',
+    
+}
+
+
+
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'TEXTFILE_METAFILE' 'dlctext.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'jdmrimscontentunlocks.meta'
+
+
+
