8ceabfbdcedb250c28b7d9113768191f8f919f9f
Ben // Zerofour // 04 <brubiyt@gmail.com>
2022-08-28 19:40:54 +0200

    Fixed resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,14 +1,12 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
  
 files {
-
     'carcols.meta',
     'dlctext.meta',
     'jdmrimscontentunlocks.meta',
     'content.xml',
     'setup2.xml',
     'global.gxt2',
-    
 }
 
 
